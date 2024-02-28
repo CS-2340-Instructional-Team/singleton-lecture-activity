b512676b2264ec093bfb14a109808b22a7f10a18
mdave1220 <156947122+mdave1220@users.noreply.github.com>
2024-02-28 16:50:26 -0500

    Update Shop.java

--- app/src/main/java/com/example/myapplication/Shop.java
+++ app/src/main/java/com/example/myapplication/Shop.java
@@ -6,17 +6,28 @@ public class Shop {
     private ArrayList<Weapon> weapon_list;
 
     // TODO: Instance should be named "shop"
+    private static Shop shop;
 
     /*
      * TODO: Constructor for Shop
      * Initialize weapon_list to an empty ArrayList
      */
+    private Shop() {
+        this.weapon_list = new ArrayList<Weapon>();
+    }
 
     /*
      * TODO: Returns the instance of the player
      * Name: getInstance()
+     * 
      * @return the instance of the shop
      */
+    public static Shop getInstance() {
+        if (shop == null) {
+            shop = new Shop();
+        }
+        return shop;
+    }
 
     public void add_weapon(String name, int damage) {
 
